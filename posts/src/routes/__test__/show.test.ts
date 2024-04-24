54a18229be4f3507f6ca5b36ed90dec7bd99c02b
tatevik-97 <tatevike9@gmail.com>
2024-04-24 20:12:29 +0400

    tests

--- posts/src/routes/__test__/show.test.ts
+++ posts/src/routes/__test__/show.test.ts
@@ -8,3 +8,16 @@ it("has a route handler listening to /api/posts/:id for get request", async () =
 
     expect(response.status).not.toEqual(404);
 });
+
+it("returns the post with the given ID if it exists", async () => {
+
+    const post = await Post.create({ title: "Test Post", content: "Test Content", userId:"testId"});
+
+    const response = await request(app).get(`/api/posts/${post.id}`).send();
+
+    expect(response.status).not.toEqual(404);
+
+    expect(response.body.title).toEqual("Test Post");
+    expect(response.body.content).toEqual("Test Content");
+});
+
