1f3217f44b87445517a3ad32447ea08739d7e6c9
tatevik-97 <tatevike9@gmail.com>
2024-05-06 19:54:20 +0400

    questions-depl.yaml

--- questions/src/api/controllers/questions.ts
+++ questions/src/api/controllers/questions.ts
@@ -1,9 +1,7 @@
 import  { Request, Response } from "express";
-import {PostsService} from "../../services/Questions";
 
 export const questions = async (req: Request, res: Response) => {
-    const allPosts = PostsService.getAllPosts();
-    // res.json(questions)
+
     res.json({message: 'Questions route'})
 
 }
